10367b3e49d873dc4d8935fd9acdef295d438992
Project V2 <brandonks337@gmail.com>
2024-02-26 21:28:14 -0600

    modified the message to tesat that pipeline is working for AWS cloud server connection to github.

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express()
 const port = 8000
 
 
+require('dotenv').config
+
+
 app.use(express.json())
 
 app.use('/', (req, res) => {
-    res.send({ status: 200, message: "Hello World"})
+    res.send({ status: 200, message: "Hello World I am the best express app ever and totally working as intended."})
 })
 
 app.listen(port, () => {
